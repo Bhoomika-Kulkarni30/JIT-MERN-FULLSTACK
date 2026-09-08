cfb15147a4d2d812745c95c37711868ff8364aba
Bhoomika-Kulkarni30 <bhoomikakulkarni104@gmail.com>
2026-09-08 16:26:30 +0530

    Project Start

---
+++ Student-TaskManagement-portal/src/components/Welcome.jsx
@@ -0,0 +1,12 @@
+export function Welcome(){
+    return (
+        <h1>
+            <center>
+
+            Welcome to Student Task Management Portal
+
+            </center>
+        </h1>
+    );
+}
+
